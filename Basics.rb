011be5b0ed98d48871784d0fcd013b1a566c4a3d
Aditi Green <asaki444@gmail.com>
2016-09-05 21:47:25 -0400

    firstexerciseoffirstchapter

---
+++ Basics.rb
@@ -0,0 +1,38 @@
+#Add two strings together that, when concatenated, return your first and last name as your full name in one string.
+"Aditi" + "Green"
+#Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the:
+4321/1000
+#returns 4
+4321/100 % 40
+#returns 3
+4321/10 % 43
+#returns 2
+4321%4320
+#returns 1
+
+
+#Write a program that uses a hash to store a list of movie titles with the year they came out. Then use the puts command to make your program print out the year of each movie to the screen. The output for your program should look something like this.
+moviehash = {:movie1 => 1975, :movie2=>2004, :movie3=>2013, :movie4=>2001, :movie5=>1981}
+puts moviehash[:movie1]
+puts moviehash[:movie2]
+puts moviehash[:movie3]
+puts moviehash[:movie4]
+puts moviehash[:movie5]
+
+moviehasharr = [1975,2004,2013,2001,1981]
+puts moviehasharr[0]
+puts moviehasharr[1]
+puts moviehasharr[2]
+puts moviehasharr[3]
+puts moviehasharr[4]
+
+#Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+puts 2.5 ** 2
+puts 3.3 ** 2
+puts 4.5 ** 2
+
+#What does the following error message tell you?
+=begin
+it says that there was a closing parentheses instead of a bracket. so it is possible that the parantheses has to be replaced with the bracket.
+=end
