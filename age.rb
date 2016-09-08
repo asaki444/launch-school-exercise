b8ef9fbce5730318903cf99cd504c87176487674
Aditi Green <asaki444@gmail.com>
2016-09-07 22:18:28 -0400

    variables exercises

---
+++ age.rb
@@ -0,0 +1,10 @@
+puts "what's your age?"
+user_age = gets.chomp
+user_age = user_age.to_i + 10
+puts "in 10 years you will be" + user_age.to_s
+user_age = user_age.to_i + 10
+puts "in 20 years you will be" + user_age.to_s
+user_age = user_age.to_i + 10
+puts "in 30 years you will be" + user_age.to_s
+user_age = user_age.to_i + 10
+puts "in 40 years you will be" + user_age.to_s
---
+++ name.rb
@@ -0,0 +1,10 @@
+puts "what's your first name?"
+user_input = gets.chomp
+puts "what's your last name?"
+user_lastname = gets.chomp
+
+10.times do puts user_input end
+
+puts user_input +" "+user_lastname
+
+ 
