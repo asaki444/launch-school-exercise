73e95455e2e967585d9e7dcca8da624447b9bd37
Aditi Green <asaki444@gmail.com>
2016-09-09 13:50:59 -0400

    methods

---
+++ greeting.rb
@@ -0,0 +1,20 @@
+def greeting(name)
+  puts "Hi" + " " +name
+end
+greeting("aditi")
+
+=begin
+question 4
+1. x = 2
+2
+2. puts x = 2
+2
+nil
+3. p name = "Joe"
+"Joe"
+4. four = "four"
+"four"
+5. print something = "nothing"
+something nil
+
+=end
---
+++ words.rb
@@ -0,0 +1,11 @@
+#question 4 prints nil
+
+def scream(words)
+  words = words + "!!!!"
+  return  words
+end
+
+scream("Yippeee")
+#Yippee!!!!
+
+#question 6 = there are not enough arguments for the method. it needs 2!
